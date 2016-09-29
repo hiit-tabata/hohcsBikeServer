4cbf81c6f6e6983f171425935f375fe85f53a986
holman <holman@live.hk>
2016-09-30 00:34:33 +0800

    fix err of default account

--- server/boot/adminAccount.js
+++ server/boot/adminAccount.js
@@ -2,18 +2,14 @@
 
 
 module.exports = function(app) {
-  var User = app.models.user;
-  var Role = app.models.Role;
-  var RoleMapping = app.models.RoleMapping;
-  var Team = app.models.Team;
+  var User = app.models.User;
 
   User.create([
     {username: 'admin', email: 'admin@holmantai.net', password: 'adminHolmantai'}
   ], function(err, users) {
-    if (err) throw err;
+    console.log(err);
 
     console.log('Created users:', users);
-
-
   });
+
 };
