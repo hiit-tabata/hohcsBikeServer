e52102fb9a273db9aafc8b0fe1767aad2bcc4c18
holman <holman@live.hk>
2016-10-11 00:19:52 +0800

    update admin

--- server/boot/adminAccount.js
+++ server/boot/adminAccount.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
   var User = app.models.User;
 
   User.create([
-    {username: 'admin', email: 'admin@holmantai.net', password: 'adminHolmantai'},
+    {username: 'admin1', email: 'admin@holmantai.net', password: 'adminHolmantai'},
     {username: 'admin', email: 'admin@admin.admin', password: 'admin'}
   ], function(err, users) {
     console.log(err);
